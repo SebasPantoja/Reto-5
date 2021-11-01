9ee6f7e2c335e094fa499eb1c305067e5da1e6ea
SebasPantoja <juan.pantoja.mt@correo.usa.edu.co>
2021-10-31 20:05:58 -0500

    first commit

---
+++ src/main/java/Reto_4/CategoriaInterface.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Reto_4;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author USUARIO
+ */
+public interface CategoriaInterface extends CrudRepository<Categoria,Integer>{
+    
+}
